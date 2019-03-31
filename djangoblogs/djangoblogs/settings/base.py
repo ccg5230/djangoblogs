e701cd2af9c22f0c9db85715fbbb056d0d1f10e4
ccg5230 <ccg5230@163.com>
2019-03-31 23:53:38 +0800

    创建blogs-博客，config-配置，comment-评论 app

--- djangoblogs/djangoblogs/settings/base.py
+++ djangoblogs/djangoblogs/settings/base.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'blogs',
+    'config',
+    'comment',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
